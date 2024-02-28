be55fc1584d94ed1a857596b50cc7ceb5cc6e829
Ronny Rook <info@ronnyrook.nl>
2024-02-28 12:04:01 +0100

    Change code styling in List

--- src/components/common/layout/List.tsx
+++ src/components/common/layout/List.tsx
@@ -1,24 +1,24 @@
-import { cn } from 'utils';
-
-type ListType = {
-  className?: string;
-  children: React.ReactNode;
-};
+import { cn } from '@utils';
 
 const ListContainer = ({ className, children }: ListType) => (
-  <ul className={cn(`flex flex-wrap list-none p-0`, className)}>{children}</ul>
+  <ul className={cn(`flex list-none flex-wrap p-0`, className)}>{children}</ul>
 );
 
 const ListItem = ({ className, children }: ListType) => (
   <li
     className={cn(
-      `py-2 px-4 m-2 rounded-md border-2 border-slate-300 bg-slate-100 dark:bg-[#222222]`,
+      `m-2 rounded-md border-2 border-slate-300 bg-slate-100 px-4 py-2 dark:bg-[#222222]`,
       className,
     )}
   >
     {children}
   </li>
 );
+
+type ListType = {
+  className?: string;
+  children: React.ReactNode;
+};
 
 export const List = {
   Container: ListContainer,
